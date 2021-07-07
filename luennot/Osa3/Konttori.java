3f1e84319ed35ab5fa041d65ae288175c2527249
petehuttunen <44609248+petehuttunen@users.noreply.github.com>
2021-07-07 21:07:17 +0300

    Add files via upload

--- luennot/Osa3/Konttori.java
+++ luennot/Osa3/Konttori.java
@@ -1,22 +1,19 @@
 package pankki;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Henkilo extends AbstractPersistable<Long> {
+@Data
+public class Konttori extends AbstractPersistable<Long> {
 
-    private String nimi;
-    @ManyToMany
-    private List<Tili> tilit = new ArrayList<>();
-
+    private String osoite;
+    @ManyToOne
+    private Pankki pankki;
 }
