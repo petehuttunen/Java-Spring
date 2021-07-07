0459c0d5fea2cab790b966bfecf7295bec0599b8
petehuttunen <44609248+petehuttunen@users.noreply.github.com>
2021-07-07 15:44:34 +0300

    Add files via upload

--- luennot/Osa2/thymeleafdata/ListaController.java
+++ luennot/Osa2/thymeleafdata/ListaController.java
@@ -1,1 +1,25 @@
+package thymeleafdata;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ListaController {
+    private List<String> lista;
+
+    public ListaController() {
+        this.lista = new ArrayList<>();
+        this.lista.add("Hello world!");
+        this.lista.add("+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.");
+    }
+
+    @GetMapping(value = "/")
+    public String home(Model model) {
+        model.addAttribute("lista", lista);
+        return "index";
+    }
+}
