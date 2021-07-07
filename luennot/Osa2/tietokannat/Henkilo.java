3d618bfe622e8796003cc365509924e856ee2be4
petehuttunen <44609248+petehuttunen@users.noreply.github.com>
2021-07-07 15:45:50 +0300

    Add files via upload

--- luennot/Osa2/tietokannat/Henkilo.java
+++ luennot/Osa2/tietokannat/Henkilo.java
@@ -1,1 +1,23 @@
+package tietokannat;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import uudelleenohjaus.GetMapping;
+import uudelleenohjaus.Model;
+import uudelleenohjaus.PostMapping;
+import uudelleenohjaus.RequestParam;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Henkilo extends AbstractPersistable<Long> {
+
+    private String nimi;
+}
---
+++ luennot/Osa2/tietokannat/HenkiloRepository.java
@@ -0,0 +1,6 @@
+import tietokannat.Henkilo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HenkiloRepository extends JpaRepository<Henkilo, Long> {
+
+}
