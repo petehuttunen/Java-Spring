28e3967107b46ba078637bfef90dd084309114fc
petehuttunen <44609248+petehuttunen@users.noreply.github.com>
2021-07-07 16:04:32 +0300

    Add files via upload

--- luennot/Osa2/uudelleenohjaus/RedirectOnPostController.java
+++ luennot/Osa2/uudelleenohjaus/RedirectOnPostController.java
@@ -1,1 +1,27 @@
+package uudelleenohjaus;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RedirectOnPostController {
+
+    private String message;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", message);
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String post(@RequestParam String content) {
+        this.message = content;
+        return "redirect:/";
+    }
+}
